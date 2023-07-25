28ef12bb48b7a3e4d1f420c6fe9ef21c8b4cf636
tsunokake <r.tsunokake.sys22@morijyobi.ac.jp>
2023-07-25 21:21:39 +0900

    修正

--- app.py
+++ app.py
@@ -1,59 +1,142 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import db, string, random
-
+import db
 
 app = Flask(__name__)
 
-# レイアウトサンプル
-@app.route('/')
-def sample_top():
-    return render_template('index.html')
+@app.route('/', methods=['GET'])
+def index():
+    msg = request.args.get('msg')
 
-@app.route('/list')
-def sample_list():
-    book_list = [
-        ('よく分かるPython', '佐々木 磨生', 'MCL出版', 200),
-        ('LinuC 詳解', '細川 潤哉', 'MCL出版', 400),
-        ('Servlet 入門', '高橋 洋平', 'ジョビ出版', 250),
-        ('Flask 入門', '高橋 洋平', 'ジョビ出版', 150),
-        ('よく分かるUML', '細川 潤哉', 'MCL出版', 220),
-        ('Django 入門', '佐々木 磨生', '龍澤出版', 350),
-    ]
-    return render_template('list.html', books=book_list)
+    if msg == None:
+        return render_template('index.html')
+    else :
+        return render_template('index.html', msg=msg)
 
-@app.route('/register')
-def sample_register():
-    return render_template('register.html')
-
-@app.route('/register_exe',methods=['POST'])
-def register_exe():
-    title = request.form.get('title')
-    author = request.form.get('author')
-    publisher = request.form.get('publisher')
-    pages = request.form.get('pages')
-    
-    db.insert_book(title,author,publisher,pages)
-    
-    book_list = db.select_all_books()
-    
-    return render_template('list.html',books=book_list)
-    
 @app.route('/', methods=['POST'])
 def login():
-    user_name=request.form.get('username')
-    password=request.form.get('password')    
-    
-    if db.login(user_name,password):
-        session['user'] = True
+    user_name = request.form.get('username')
+    password = request.form.get('password')
+
+   
+    if db.login(user_name, password):
         return redirect(url_for('mypage'))
-    else:
-        error='ユーザー名またはパスワードが違います'
-        input_data={'user_name':user_name, 'password':password}
-        return render_template('index.html',error=error,data=input_data)
+    else :
+        error = 'ユーザ名またはパスワードが違います。'
+
+       
+        input_data = {'user_name':user_name, 'password':password}
+        return render_template('index.html', error=error, data=input_data)
     
+   
+@app.route("/logout")
+def logout():
+    return render_template('index.html ')
+
+
 @app.route('/mypage', methods=['GET'])
 def mypage():
     return render_template('mypage.html')
+
+@app.route('/register')
+def register_form():
+    return render_template('register.html')
+
+@app.route('/register_exe', methods=['POST'])
+def register_exe():
+    user_name = request.form.get('username')
+    password = request.form.get('password')
+
+    if user_name == '':
+        error = 'ユーザ名が未入力です。'
+        return render_template('register.html', error=error, user_name=user_name, password=password)
+    if password == '':
+        error = 'パスワードが未入力です。'
+        return render_template('register.html', error=error)
+
+    count = db.insert_user(user_name, password)
+
+    if count == 1:
+        msg = '登録が完了しました。'
+        return redirect(url_for('index', msg=msg))
+    else:
+        error = '登録に失敗しました。'
+        return render_template('register.html', error=error)
     
-if __name__ == "__main__":
+    
+@app.route('/Boo_list')
+def Boo_list():
+    book_list = db.lis_boo()
+    return render_template('Book_list.html',books = book_list)
+
+
+@app.route('/hamed')
+def home_back():
+    return render_template('index.html')
+
+
+@app.route('/regi_book')
+def register_book():
+    return render_template('register_Book.html')
+
+@app.route('/news_Book', methods=['POST'])
+def new_Book():
+    title = request.form.get('title')
+    author = request.form.get('author')
+    
+    pages = request.form.get('pages')
+    
+    db.insert_book(title, author, pages)
+    
+    book_list = db.lis_boo()
+    return render_template('success_Book.html', books=book_list)
+
+
+@app.route('/h1_back')
+def h1_back():
+    return render_template('mypage.html')
+
+
+
+
+#BOOKの削除
+@app.route('/kes_bo')
+def kes_bo():
+    return render_template('delete_Book.html')
+
+@app.route("/delete_book", methods=["POST"])
+def delete_book():
+    title = request.form.get("title")
+
+    db.dele_book(title)
+
+    return render_template("delete_ok.html")
+
+#戻る
+@app.route('/h2_back')
+def h2_back():
+    return render_template('mypage.html')
+
+#検索
+@app.route('/serch')
+def serch():
+    return render_template('search_form.html')
+
+@app.route("/search_book", methods=["POST"])
+def search_book():
+    title = request.form.get("title")
+
+    book_list = db.book_search(title)
+
+    return render_template("search_books.html", books=book_list)
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
--- db.py
+++ db.py
@@ -1,31 +1,144 @@
-import os
-import psycopg2
+import os, psycopg2, string, random, hashlib
 
 def get_connection():
     url = os.environ['DATABASE_URL']
     connection = psycopg2.connect(url)
     return connection
 
-def select_all_books():
+def get_salt():
+    charset = string.ascii_letters + string.digits
+
+    salt = ''.join(random.choices(charset, k=30))
+    return salt
+
+def get_hash(password, salt):
+    b_pw = bytes(password, 'utf-8')
+    b_salt = bytes(salt, 'utf-8')
+    hashed_password = hashlib.pbkdf2_hmac('sha256', b_pw, b_salt, 1246).hex()
+    return hashed_password
+
+def insert_user(user_name, password):
+    sql = 'INSERT INTO user_sample VALUES(default, %s, %s, %s)'
+
+    salt = get_salt()
+    hashed_password = get_hash(password, salt)
+
+    try :
+        connection = get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute(sql, (user_name, hashed_password, salt))
+        count = cursor.rowcount 
+        connection.commit()
+
+    except psycopg2.DatabaseError :
+        count = 0
+
+    finally :
+        cursor.close()
+        connection.close()
+
+    return count
+
+def login(user_name, password):
+    sql = 'SELECT hashed_password, salt FROM user_sample WHERE name = %s'
+    flg = False
+
+    try :
+        connection = get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute(sql, (user_name,))
+        user = cursor.fetchone()
+
+        if user != None:
+            
+            salt = user[1]
+
+           
+            hashed_password = get_hash(password, salt)
+
+          
+            if hashed_password == user[0]:
+                flg = True
+
+    except psycopg2.DatabaseError :
+        flg = False
+
+    finally :
+        cursor.close()
+        connection.close()
+
+    return flg
+
+
+def select_all_book(title, author, pages):
+    connetiion = get_connection()
+    cursor =connetiion.cursor()
+    sql = 'INSERT INTO book_sample VALUES (default, %s, %s, %s)'
+    
+    cursor.execute(sql, (title, author, pages ))
+    
+    connetiion.commit()
+    cursor.close()
+    connetiion.close()
+
+
+def insert_book(title, author, pages):
+    connetiion = get_connection()
+    cursor =connetiion.cursor()
+    sql = 'INSERT INTO book_sample VALUES (default, %s, %s, %s)'
+    
+    cursor.execute(sql, (title, author, pages ))
+    
+    connetiion.commit()
+    cursor.close()
+    connetiion.close()
+    
+
+def dele_book(title):
+    connection = get_connection()
+    cursor = connection.cursor()
+    sql = "delete from book_sample where title=%s"
+
+    cursor.execute(sql, (title,))
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+    
+    
+def lis_boo():
     connection = get_connection()
     cursor = connection.cursor()
 
-    sql = 'SELECT title, author, publisher, pages FROM books_sample'
+    sql = "SELECT title, author, pages FROM book_sample"
 
     cursor.execute(sql)
+
     rows = cursor.fetchall()
 
     cursor.close()
     connection.close()
+
     return rows
 
-def insert_book(title,author,publisher,pages):
+def book_search(title):
     connection = get_connection()
     cursor = connection.cursor()
-    sql = 'INSERT INTO books_sample VALUES (default,%s,%s,%s,%s)'
-    
-    cursor.execute(sql,(title,author,publisher,pages))
-    
-    connection.commit()
+
+    sql = "SELECT * FROM book_sample WHERE title LIKE %s"
+
+    titles = "%" + title + "%"
+
+    cursor.execute(sql, (title,))
+
+    rows = cursor.fetchall()
+
     cursor.close()
     connection.close()
+
+    return rows
+    
+    
+    
